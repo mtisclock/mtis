7cfc047240d79db646163e24beda809451eb1eb4
mtisclock <247edinburgh@googlemail.com>
2026-09-26 16:37:02 +0100

    Update events.js

--- events.js
+++ events.js
@@ -60,7 +60,7 @@ const events = [
   },
   {
     date: "2026-09-22T00:00:00+01:00",
-    title: "Estimate 1 and survey received"
+    title: "Building condition survey and cost estimate 1 received"
   },
   {
     date: "2026-09-23T00:00:00+01:00",
