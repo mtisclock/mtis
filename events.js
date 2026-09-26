4ed396422f94b0a4ca830a196f07ff33f31ac76e
mtisclock <247edinburgh@googlemail.com>
2026-09-26 16:33:13 +0100

    Update events.js

--- events.js
+++ events.js
@@ -1,22 +1,22 @@
 const events = [
   {
     date: "2023-07-26T00:00:00+01:00",
-    title: "Council said:",
+    title: "Council letter:",
     quote: "The surveys will be carried out in the coming weeks."
   },
   {
     date: "2023-11-01T00:00:00+00:00",
-    title: "Delay announced:",
+    title: "Council letter - first delay announced:",
     quote: "Due to your property being of different build, there is a delay to spring 2025."
   },
   {
     date: "2024-01-29T00:00:00+00:00",
-    title: "Council said:",
+    title: "Council letter:",
     quote: "We are pleased to confirm to you that we are in a position to recommence building condition surveys on your block. Once the surveys are completed we will issue these to you as quickly as possible."
   },
   {
     date: "2024-02-22T00:00:00+00:00",
-    title: "Council said:",
+    title: "Council letter:",
     quote: "We have contracted Thomas and Adamson surveyors to carry out the surveys in the coming weeks."
   },
   {
@@ -25,22 +25,22 @@ const events = [
   },
   {
     date: "2024-06-27T00:00:00+01:00",
-    title: "Council announces new dates for Phase 6:",
-    quote: "It is with great delight that we are able to share the programme for 2024/25 and 2025/26. Your Phase 6 has a March 25 construction start and Dec 25 construction finish."
+    title: "Council announcement - new dates for Phase 6:",
+    quote: "It is with great delight that we are able to share the programme. Your Phase 6 has a March 25 construction start and Dec 25 construction finish."
   },
   {
     date: "2024-11-22T00:00:00+00:00",
-    title: "Council says there is a further delay:",
+    title: "Council letter - nope, another delay:",
     quote: "We have identified that your home has been built in a different way to most homes in your area. It has become apparent that the potential solution [for your home] would be too expensive and not affordable. We will trial a different approach. We are still working towards fulfilling the 2025/26 projected timescale."
   },
   {
     date: "2024-12-23T00:00:00+00:00",
-    title: "Council said:",
+    title: "Council letter:",
     quote: "Council personnel will be visiting the area week beginning the 16 January 2025 to carry out final inspections and may attend your property."
   },
   {
     date: "2025-11-27T00:00:00+00:00",
-    title: "Council said:",
+    title: "Council letter:",
     quote: "Phase 6 pre construction procurement and consultation are due to begin autumn 2026."
   },
   {
@@ -55,7 +55,7 @@ const events = [
   },
   {
     date: "2026-06-12T00:00:00+01:00",
-    title: "Council said:",
+    title: "Council letter:",
     quote: "We will be in a position to issue cost estimate 1 to you late August along with your building condition survey."
   },
   {
@@ -64,7 +64,7 @@ const events = [
   },
   {
     date: "2026-09-23T00:00:00+01:00",
-    title: "I voted against the works"
+    title: "I have voted against the works"
   }
 ];
 
